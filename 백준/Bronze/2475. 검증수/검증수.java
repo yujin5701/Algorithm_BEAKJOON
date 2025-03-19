ea2481351b9cf602fdd8d650c85e438d19f91987
Yujin Gung <yoojin3117@ewhain.net>
2025-03-19 18:35:11 +0900

    [Bronze V] Title: 검증수, Time: 180 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int num = sc.nextInt();
+            num = num * num;
+            sum = sum + num;
+        }
+        System.out.println(sum % 10);
+        sc.close();
+
+    }
+}
