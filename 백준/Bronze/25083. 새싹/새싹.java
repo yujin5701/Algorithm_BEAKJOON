59aba5120c4d238bc7e81887b2fb2387d8332868
Yujin Gung <yoojin3117@ewhain.net>
2025-03-20 20:07:53 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("         ,r'");
+        System.out.println("\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
