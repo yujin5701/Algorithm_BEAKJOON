34947ccccdba3c08e7b320e59167cde07b549555
Yujin Gung <yoojin3117@ewhain.net>
2025-06-29 21:57:32 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 776 ms, Memory: 236752 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) + "\n");
+
+        }
+        bw.close();
+    }
+}
