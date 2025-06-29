9521a373f2d63af46e9dc91844f44eaebcb03344
Yujin Gung <yoojin3117@ewhain.net>
2025-06-29 22:03:53 +0900

    [Bronze V] Title: 개수 세기, Time: 188 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), count = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int num = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == num)
+                count++;
+        }
+        System.out.println(count);
+
+    }
+}
