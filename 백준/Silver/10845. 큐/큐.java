218ea57346605444c60f7456193b0ae8044d2841
yujin5701 <yoojin3117@ewhain.net>
2025-08-30 00:15:39 +0900

    [Silver IV] Title: 큐, Time: 288 ms, Memory: 19912 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,73 +1,51 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void push(ArrayList<Integer> arr, int n) {
-        arr.add(n);
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> queue = new ArrayDeque<>();
 
-    public static int pop(ArrayList<Integer> arr) {
-        if (arr.isEmpty())
-            return -1;
-        int n = arr.get(0);
-        arr.remove(0);
-        return n;
-    }
-
-    public static int size(ArrayList<Integer> arr) {
-        return arr.size();
-    }
-
-    public static int empty(ArrayList<Integer> arr) {
-        if (arr.isEmpty())
-            return 1;
-        else
-            return 0;
-    }
-
-    public static int front(ArrayList<Integer> arr) {
-        if (arr.isEmpty())
-            return -1;
-        return arr.get(0);
-    }
-
-    public static int back(ArrayList<Integer> arr) {
-        if (arr.isEmpty())
-            return -1;
-        return arr.get(arr.size() - 1);
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            String s = sc.next();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
             switch (s) {
                 case "push":
-                    int num = sc.nextInt();
-                    push(arr, num);
+                    int num = Integer.parseInt(st.nextToken());
+                    queue.add(num);
                     break;
                 case "pop":
-                    System.out.println(pop(arr));
+                    if (queue.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        int a = queue.poll();
+                        System.out.println(a);
+                    }
                     break;
                 case "size":
-                    System.out.println(size(arr));
+                    System.out.println(queue.size());
                     break;
                 case "empty":
-                    System.out.println(empty(arr));
+                    if(queue.isEmpty()){
+                        System.out.println(1);
+                    }
+                    else System.out.println(0);
+                    
                     break;
                 case "front":
-                    System.out.println(front(arr));
+                if(queue.isEmpty()) System.out.println(-1);
+                else System.out.println(queue.peekFirst());
                     break;
                 case "back":
-                    System.out.println(back(arr));
+                if(queue.isEmpty()) System.out.println(-1);
+                else System.out.println(queue.peekLast());
                     break;
                 default:
                     break;
             }
+
         }
-        sc.close();
 
     }
 }
