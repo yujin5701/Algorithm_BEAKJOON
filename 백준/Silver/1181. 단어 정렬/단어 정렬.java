133f804704c4bbdee9a38183135ba4990bbd4b36
yujin5701 <yoojin3117@ewhain.net>
2025-09-02 17:31:50 +0900

    [Silver V] Title: 단어 정렬, Time: 308 ms, Memory: 25824 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,56 +1,54 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-
-class Word implements Comparable<Word> {
-    String content;
-    int len;
-
-    public Word(String content, int len) {
-        this.content = content;
-        this.len = len;
-    }
-
-    @Override
-    public int compareTo(Word o) {
-        if (this.len > o.len)
-            return 1;
-        else if (this.len == o.len) {
-            int i = 0;
-            if (this.content.equals(o.content))
-                return 1;
-            while (this.content.charAt(i) == o.content.charAt(i)) {
-                i++;
-            }
-            if (this.content.charAt(i) > o.content.charAt(i))
-                return 1;
-        }
-        return -1;
-    }
-}
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        ArrayList<Word> words = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String w = sc.next();
-            words.add(new Word(w, w.length()));
+    public static void mergeSort(String[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (right + left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
         }
+    }
 
-        Collections.sort(words);
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && words.get(i).content.equals(words.get(i - 1).content)) {
+    public static void merge(String[] arr, int left, int mid, int right) {
+        String[] temp = new String[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
 
+        while (i <= mid && j <= right) {
+            if (arr[i].length() > arr[j].length()) {
+                temp[k++] = arr[j++];
+            } else if (arr[i].length() < arr[j].length()) {
+                temp[k++] = arr[i++];
             } else {
-                System.out.println(words.get(i).content);
+                if(arr[i].compareTo(arr[j]) <= 0){
+                    temp[k++] = arr[i++];
+                }
+                else temp[k++] = arr[j++];
             }
-
         }
-        sc.close();
+        while (i <= mid)
+            temp[k++] = arr[i++];
+        while (j <= right)
+            temp[k++] = arr[j++];
+        for (int a = 0; a < temp.length; a++) {
+            arr[left + a] = temp[a];
+        }
+    }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+        String[] arr = set.toArray(new String[0]);
+        mergeSort(arr, 0, arr.length - 1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
