3c97959c9e075a6919dfe39628126e643191be33
yujin5701 <yoojin3117@ewhain.net>
2025-09-03 14:17:45 +0900

    [Silver V] Title: 숫자 카드, Time: 1144 ms, Memory: 114104 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int binartSearch(List<Integer> list, int target) {
+        int left = 0, right = list.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) == target)
+                return 1;
+            else if (list.get(mid) < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return 0;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            sb.append(binartSearch(list, Integer.parseInt(st.nextToken()))).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
