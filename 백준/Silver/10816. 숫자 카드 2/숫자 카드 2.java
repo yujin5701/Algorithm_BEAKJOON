f59755feb46e08d3f83eb67a3005854ea200a171
yujin5701 <yoojin3117@ewhain.net>
2025-09-07 22:11:39 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1028 ms, Memory: 172948 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,43 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int lowerBound(List<Integer> list, int target){
-        int left = 0, right = list.size();
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if(list.get(mid) < target) left = mid + 1;
-            else right = mid;
-        }
-        return left;
-    }
-
-    public static int upperBound(List<Integer> list, int targer){
-        int left = 0, right = list.size();
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if(list.get(mid) <= targer) left = mid + 1;
-            else right = mid;
-        }
-        return left;
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        Collections.sort(list);
-
         int m = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            sb.append(upperBound(list, x) - lowerBound(list, x)).append(" ");
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(num, 0)).append(" ");
         }
+
         System.out.println(sb);
     }
 }
